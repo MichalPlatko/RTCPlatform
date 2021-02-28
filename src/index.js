f91d112749b3a31e928d2841dd29db437439e49b
MichalPlatko <miso.platko@gmail.com>
2021-03-01 00:00:32 +0100

    added individual elements

--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import ReactDOM from 'react-dom';
 import "./styles/app.css"
 import {CssBaseline} from "@material-ui/core";
 
+import LeftSidebar from "./components/leftsidebar";
 import Sidebar from "./components/sidebar";
-
+import Chat from "./components/chat";
+import RightSidebar from "./components/rightsidebar";
 
 class App extends React.Component{
   render(){
     return(
-      <>
-      <CssBaseline />
       <div className="App">
+      <CssBaseline />
+      <LeftSidebar/>
       <Sidebar/>
+      <Chat/>
+      <RightSidebar/>
       </div>
-
-      </>
     )
   }
 }
@@ -26,8 +28,9 @@ ReactDOM.render(<App/>,document.getElementById('root'));
 
 /**
  * DISCORD THEME COLORS
- * #2f3136 left panel
+ * #202225 left panel
  * #2f3136 left sidebar
+ * #292b2f profile menu
  * #36393f message box
  * #2f3136 right sidebar
  * #202225 top bar

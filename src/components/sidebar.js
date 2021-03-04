bbce236ac4e95f02cdbdfa25f5eb173f4929d909
MichalPlatko <miso.platko@gmail.com>
2021-03-04 12:57:25 +0100

    shows data fetched from db

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import "../styles/sidebar.css";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AddIcon from '@material-ui/icons/AddSharp';
@@ -11,8 +11,33 @@ import MicOffIcon from '@material-ui/icons/MicOff';
 import HeadsetIcon from '@material-ui/icons/Headset';
 import SettingsIcon from '@material-ui/icons/Settings';
 
-class SideBar extends React.Component{
-    render(){
+import {useSelector,useDispatch} from "react-redux";
+import {selectUser} from "../userSlice";
+import db from '../firebaseconfig';
+import {setChannelInfo} from "../appSlice";
+
+function SideBar(){
+    const user = useSelector(selectUser);
+    const [channels,setChannels] = useState([]);
+    useEffect(()=>{
+        //if anything changes->onSnapshot
+        db.collection("channels").onSnapshot(snapshot=>{
+            //each doc return object
+            setChannels(snapshot.docs.map(doc=>({
+            id:doc.id,
+            channel:doc.data(),
+            })));
+        })
+    },[]);
+
+    const addChannel=()=>{
+    const channelName=prompt("Add Channel Name");
+    if(channelName){
+        db.collection("channels").add({
+            name:channelName
+        });
+    }
+    }
     return (
         <div className="sidebar"> 
         
@@ -27,16 +52,11 @@ class SideBar extends React.Component{
         <ExpandMoreIcon/>
         <h4>Text Channels</h4>
         </div>
-        <AddIcon className="sidebar_addChannel" />
+        <AddIcon onClick={addChannel} className="sidebar_addChannel" />
         </div>
 
         <div className="sidebar_channelsList">
-        <SidebarChannel/>
-        <SidebarChannel/>
-        <SidebarChannel/>
-        <SidebarChannel/>
-        <SidebarChannel/>
-        <SidebarChannel/>
+        {channels.map(c=>(<SidebarChannel key={c.id} id={c.id} name={c.channel.name}/>))}
         </div>
         </div>
 
@@ -52,27 +72,20 @@ class SideBar extends React.Component{
         </div>
         </div>
 
-        <SidebarProfile/>
+        <SidebarProfile  photo={user.photo} userName={user.displayName} tag={user.uid} />
 
         </div>
     )
-    }
 }
 
 
-class SidebarProfile extends React.Component {
-    constructor(props){
-    super(props);
-    this.src=this.props.src;
-    this.user=this.props.user;
-    }
-    render(){
+function SidebarProfile({photo,userName,tag}){
     return (
         <div className="sidebar_profile">
-        <Avatar className="sidebar_profileAvatar">M</Avatar>
+        <Avatar className="sidebar_profileAvatar" src={photo}></Avatar>
         <div className="sidebar_profileInfo">
-        <h3>UserName</h3>
-        <p>#TAG</p>
+        <h3>{userName}</h3>
+        <p>#{tag.substring(0,5)} </p>
         </div>
         <div className="sidebar_profileIcons">
         <MicIcon/>
@@ -81,25 +94,24 @@ class SidebarProfile extends React.Component {
         </div>
         </div>
     )
-    }
 }
 
-class SidebarChannel extends React.Component{
-    constructor(props){
-        super(props);
-        this.id=props.id;
-        this.name=props.name;
-    }
-    render(){
+function SidebarChannel({id,name}){
+    const dispatch=useDispatch();
+
     return (
-        <div className="sidebarChannel">
+        <div className="sidebarChannel" onClick={()=>{
+        dispatch(setChannelInfo({
+            channelId:id,
+            channelName:name,
+        }))
+        }}>
         <h4>
         <span className="sidebarChannelHashIcon">#</span>
-        Channel name
+        {name}
         </h4>
         </div>
     )
-    }
 }
 
 export default SideBar;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import "./styles/app.css"
 import {CssBaseline} from "@material-ui/core";
@@ -7,24 +7,69 @@ import LeftSidebar from "./components/leftsidebar";
 import Sidebar from "./components/sidebar";
 import Chat from "./components/chat";
 import RightSidebar from "./components/rightsidebar";
+import LoginPage from "./components/loginpage";
 
-class App extends React.Component{
-  render(){
+import {useSelector,useDispatch,Provider} from "react-redux";
+import {selectUser,login,logout} from "./userSlice";
+
+import storeReducers from "./store";
+
+import {auth} from "./firebaseconfig";
+
+function WrappedAppInRedux(){
+  return(
+    <Provider store={storeReducers}>
+      <App/>
+    </Provider>
+  )
+}
+
+function App(){
+    const user=useSelector(selectUser);
+    const dispatch=useDispatch();
+
+    useEffect(() => {
+     auth.onAuthStateChanged((authUser)=>{
+      if(authUser){
+        //user logged in 
+        dispatch(
+        login({
+        uid:authUser.uid,
+        photo:authUser.photoURL,
+        email:authUser.email,
+        displayName:authUser.displayName
+        }));
+      }else{
+        dispatch(logout());
+
+      }
+     })
+    }, [dispatch])
     return(
       <div className="App">
+      { user ? (
+      <>
       <CssBaseline />
       <LeftSidebar/>
       <Sidebar/>
       <Chat/>
       <RightSidebar/>
+      </>
+      )
+      : (
+      <LoginPage/> 
+      )
+      }
+      
       </div>
     )
-  }
 }
 
-//TODO: REDO LAYOUT TO USE
+//TODO:ADDED REDUX PROVIDER TO HOLD ONTO STATE,
+//https://www.youtube.com/watch?v=zc1loX80TX8&list=WL&ab_channel=CleverProgrammer
+//2:31:10 -> setup chat to display message under appropriate selected channel loaded from db
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+ReactDOM.render(<WrappedAppInRedux/>,document.getElementById('root'));
 
 /**
  * DISCORD THEME COLORS

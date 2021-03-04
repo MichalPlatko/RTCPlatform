a4c46b98ec464f9e85f9da29bcdd68f4169b59b4
MichalPlatko <miso.platko@gmail.com>
2021-03-04 12:57:49 +0100

    basic login page need to be redone

---
+++ src/components/loginpage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../styles/login.css";
+import {Button} from "@material-ui/core";
+import {auth,provider} from "../firebaseconfig";
+
+class LoginPage extends React.Component{
+    signIn=()=>{
+    auth.signInWithPopup(provider)
+    .catch(error=>alert(error.message));
+    }
+    render(){
+    return (
+        <div className="login">
+        <div className="login_logo" > <img></img> </div>
+        <Button  onClick={this.signIn}>Sign in</Button>
+        </div>
+    )}
+}
+
+export default LoginPage;
